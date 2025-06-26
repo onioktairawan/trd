77ba3283891c7aefa7a5886ca4d1b13826915541
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-06-26 15:13:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 MONGO_URI = "mongodb+srv://idopop3:Irawanoni@oni.a8z7e6x.mongodb.net/?retryWrites=true&w=majority&appName=oni"
-DB_NAME = "jurnal_trading2"
+DB_NAME = "trading_jouurnal"
 COLLECTION_NAME = "trades"
+SECRET_KEY = "supersecretkey"
